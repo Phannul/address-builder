54d53327039f836676a39a7e69db25f253eee01e
phannu <fanuelbirhane433@gmail.com>
2025-09-30 09:50:08 -0500

    concluded billing prompter and started with shipping prompter

--- src/main/java/AddressBuilder.java
+++ src/main/java/AddressBuilder.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class AddressBuilder {
+    static Scanner myScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        billingPrompt(myScanner);
+
+    }
+
+    public static void billingPrompt(Scanner myScanner) {
+        System.out.println("Please provide the following Information: Fullname\n ");
+        String fullName = myScanner.nextLine();
+
+        System.out.println("Billing Street: ");
+        String billingStreet = myScanner.nextLine();
+
+        System.out.println("Billing City: ");
+        String billingCity = myScanner.nextLine();
+
+        System.out.println("Billing State: ");
+        String billingState = myScanner.nextLine();
+
+        System.out.println("Billing Zip: ");
+        String billingZip = myScanner.nextLine();
+    }
+    public static void shippingPrompt(Scanner myScanner) {
+        System.out.println("Let's start with you shipping address Shipping Address\n");
+        String shippingAddress = myScanner.nextLine();
+
+        System.out.println("Shipping");
+    }
 }
